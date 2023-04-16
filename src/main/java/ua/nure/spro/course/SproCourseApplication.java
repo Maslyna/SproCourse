ba97b82106d21cd99529dcbb928b4869671a7e3f
Maslyna <sanci324@gmail.com>
2023-04-16 18:18:44 +0200

    Init commit

---
+++ src/main/java/ua/nure/spro/course/SproCourseApplication.java
@@ -0,0 +1,13 @@
+package ua.nure.spro.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SproCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SproCourseApplication.class, args);
+    }
+
+}
